21bf33d31d99442a29ddbbc28186bc379cb8c8f4
SamBobBarnes <samuelrbarnes@outlook.com>
2025-11-07 20:17:02 -0600

    d5p3

--- Day5/Part3.cs
+++ Day5/Part3.cs
@@ -1,12 +1,170 @@
-﻿namespace everybody.codes_2025.Day5;
+﻿using System.Text;
 
-public class Part3() : BasePart(5,3,true)
+namespace everybody.codes_2025.Day5;
+
+public class Part3() : BasePart(5,3)
 {
     public override string Run()
     {
-        var input = Input();
-        //var input = InputChars();
+        var input = Input().Select(x => (Id:x.Split(":")[0], Bones:x.Split(":")[1].Split(",").Select(int.Parse).ToArray()));
 
-        return 0.ToString();
+        var fishbones = new List<Fishbone>();
+        foreach (var row in input)
+        {
+            fishbones.Add(new(row.Id, row.Bones));
+        }
+
+        var comparer = new FishboneComparer();
+        fishbones.Sort(comparer);
+        fishbones.Reverse();
+
+        var checksum = fishbones.Select((x, index) => x.Id * (index + 1)).Sum();
+
+        return checksum.ToString();
+    }
+
+    private class FishboneRib(int spine)
+    {
+        public int Spine { get; set; } = spine;
+        public int? LeftRib { get; set; }
+        public int? RightRib { get; set; }
+
+        public int Value()
+        {
+            var sb = new StringBuilder();
+
+            if (LeftRib != null)
+            {
+                sb.Append(LeftRib);
+            }
+            sb.Append(Spine);
+            if (RightRib != null)
+            {
+                sb.Append(RightRib);
+            }
+
+            return int.Parse(sb.ToString());
+        }
+
+        public override string ToString()
+        {
+            var sb = new StringBuilder();
+            if (LeftRib != null)
+            {
+                sb.Append(LeftRib);
+                sb.Append("-");
+            }
+            else
+            {
+                sb.Append("  ");
+            }
+
+            sb.Append(Spine);
+            if (RightRib != null)
+            {
+                sb.Append("-");
+                sb.Append(RightRib);
+            }
+            else
+            {
+                sb.Append("  ");
+            }
+
+            return sb.ToString();
+        }
+    }
+
+    private class Fishbone
+    {
+        private List<FishboneRib> _ribs = [];
+        public int Id { get; }
+        public int[] Rows => _ribs.Select(x => x.Value()).ToArray();
+
+        public Fishbone(string id, int[] bones)
+        {
+            Id = int.Parse(id);
+            foreach (var bone in bones)
+            {
+                AddBone(bone);
+            }
+        }
+
+        public void AddBone(int bone)
+        {
+            if(_ribs.Count == 0)
+            {
+                _ribs.Add(new(bone));
+                return;
+            }
+
+            var placed = false;
+
+            for(int i = 0; i < _ribs.Count; i++)
+            {
+                var current = _ribs[i];
+                if (bone < current.Spine && current.LeftRib == null)
+                {
+                    current.LeftRib = bone;
+                    placed = true;
+                }
+                else if (bone > current.Spine && current.RightRib == null)
+                {
+                    current.RightRib = bone;
+                    placed = true;
+                }
+
+                if (placed) break;
+            }
+
+            if (!placed)
+            {
+                _ribs.Add(new(bone));
+            }
+        }
+
+        public long Value() {
+            var value = new StringBuilder();
+            foreach (var rib in _ribs)
+            {
+                value.Append(rib.Spine);
+            }
+
+            return long.Parse(value.ToString());
+        }
+
+        public override string ToString()
+        {
+            var value = new StringBuilder($"{Id}: ");
+            foreach (var rib in _ribs)
+            {
+                value.Append(rib.Spine);
+            }
+
+            return value.ToString();
+        }
+    }
+
+    private class FishboneComparer : IComparer<Fishbone>
+    {
+        public int Compare(Fishbone? x, Fishbone? y)
+        {
+            if (x == null) return -1;
+            if (y == null) return 1;
+            if (x.Value() > y.Value()) return 1;
+            if (x.Value() < y.Value()) return -1;
+            if (x.Value() != y.Value()) throw new ArgumentOutOfRangeException();
+            var aRows = x.Rows;
+            var bRows = y.Rows;
+
+            for (int i = 0; i < aRows.Length; i++)
+            {
+                if (aRows[i] == bRows[i]) continue;
+                if (aRows[i] > bRows[i]) return 1;
+                if (aRows[i] < bRows[i]) return -1;
+            }
+
+            if (x.Id > y.Id) return 1;
+            return -1;
+        }
     }
 }
--- obj/Debug/net8.0/everybody.codes-2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/everybody.codes-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d76b001ecd21463a75d40915d8f919c47945a3f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2f3027456a27cfa36739f02cd08557f7edfdcc1d")]
 [assembly: System.Reflection.AssemblyProductAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
