b63448758bc0b8a7ecde06e01c352095af4b4bce
SamBobBarnes <sbarnes@vermeer.com>
2025-11-11 08:49:27 -0600

    d6p3

--- Day6/Part3.cs
+++ Day6/Part3.cs
@@ -1,12 +1,95 @@
 ﻿namespace everybody.codes_2025.Day6;
 
-public class Part3() : BasePart(6,3,true)
+public class Part3() : BasePart(6,3)
 {
     public override string Run()
     {
-        //var input = Input();
-        //var input = InputChars();
+        var input = InputChars();
 
-        return 0.ToString();
+        var mentorIndices = new Dictionary<char, List<int>>
+        {
+            {'A',new()},
+            {'B',new()},
+            {'C',new()}
+        };
+        var noviceIndices = new Dictionary<char, List<int>>
+        {
+            {'a',new()},
+            {'b',new()},
+            {'c',new()}
+        };
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            switch (input[i])
+            {
+                case 'A':
+                    mentorIndices['A'].Add(i);
+                    break;
+                case 'B':
+                    mentorIndices['B'].Add(i);
+                    break;
+                case 'C':
+                    mentorIndices['C'].Add(i);
+                    break;
+                case 'a':
+                    noviceIndices['a'].Add(i);
+                    break;
+                case 'b':
+                    noviceIndices['b'].Add(i);
+                    break;
+                case 'c':
+                    noviceIndices['c'].Add(i);
+                    break;
+            }
+        }
+
+        var total = 0;
+        var repetitions = 1000;
+        var distanceLimit = 1000;
+        var length = input.Length;
+        
+        var noviceMentorPairs = new List<(char Novice, char Mentor)>
+        {
+            ('a','A'),
+            ('b','B'),
+            ('c','C'),
+        };
+         
+        foreach(var pair in noviceMentorPairs)
+        {
+            var localTotal = 0;
+            var startList = mentorIndices[pair.Mentor].Select(x => x -= length).ToList();
+            var endList = mentorIndices[pair.Mentor].Select(x => x += length).ToList();
+            var fullList = new List<int>();
+            fullList.AddRange(startList);
+            fullList.AddRange(endList);
+            fullList.AddRange(mentorIndices[pair.Mentor]);
+            foreach (var noviceIndex in noviceIndices[pair.Novice])
+            {
+                localTotal += fullList.Count(x => x >= noviceIndex - distanceLimit && x <= noviceIndex + distanceLimit);
+            }
+
+            localTotal *= repetitions-2;
+            var fullStartList = new List<int>();
+            fullStartList.AddRange(startList);
+            fullStartList.AddRange(mentorIndices[pair.Mentor]);
+            var fullEndList = new List<int>();
+            fullEndList.AddRange(endList);
+            fullEndList.AddRange(mentorIndices[pair.Mentor]);
+            foreach (var noviceIndex in noviceIndices[pair.Novice])
+            {
+                localTotal += fullStartList.Count(x => x >= noviceIndex - distanceLimit && x <= noviceIndex + distanceLimit);
+            }
+            foreach (var noviceIndex in noviceIndices[pair.Novice])
+            {
+                localTotal += fullEndList.Count(x => x >= noviceIndex - distanceLimit && x <= noviceIndex + distanceLimit);
+            }
+
+            total += localTotal;
+        }
+
+
+        return total.ToString();
     }
 }
--- obj/Debug/net8.0/everybody.codes-2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/everybody.codes-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9956b8dbf0a85439d56997d5d4e27f68db940cc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4655d1574181062668074246ec43341af5603fbd")]
 [assembly: System.Reflection.AssemblyProductAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
