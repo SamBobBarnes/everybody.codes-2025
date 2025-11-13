3fed7738f18383e7dd736f69a9bc64e08fc10780
SamBobBarnes <samuelrbarnes@outlook.com>
2025-11-12 19:47:50 -0600

    d8p2

--- Day8/Part2.cs
+++ Day8/Part2.cs
@@ -1,12 +1,33 @@
 ﻿namespace everybody.codes_2025.Day8;
 
-public class Part2() : BasePart(8,2,true)
+public class Part2() : BasePart(8,2)
 {
     public override string Run()
     {
-        //var input = Input();
-        //var input = InputChars();
+        var input = Input()[0].Split(',').Select(int.Parse).ToList();
 
-        return 0.ToString();
+        var nails = 256;
+
+        var lines = new List<(int A, int B)>();
+
+        var total = 0;
+
+        for (int i = 0; i < input.Count-1; i++)
+        {
+            var a = Math.Min(input[i], input[i + 1]);
+            var b = Math.Max(input[i], input[i + 1]);
+
+            foreach (var line in lines)
+                total += AreCrossing((a, b), line) ? 1 : 0;
+
+            lines.Add((a, b));
+        }
+
+        return total.ToString();
+    }
+
+    private bool AreCrossing((int A, int B) x,(int A, int B) y)
+    {
+        return (x.A < y.A && x.B > y.A && x.B < y.B) || (x.A > y.A && x.A < y.B && x.B > y.B);
     }
 }
--- obj/Debug/net8.0/everybody.codes-2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/everybody.codes-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ef1dc2647919f7816878940295594e1787424b0f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9eb6a339225d38ce0143a74a5debb6bc1671f9b8")]
 [assembly: System.Reflection.AssemblyProductAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
