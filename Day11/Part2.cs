2d7a9f0c0afa26d32a39782cb39bdac1a9d45e06
SamBobBarnes <samuelrbarnes@outlook.com>
2025-11-17 19:26:11 -0600

    d11p2

--- Day11/Part2.cs
+++ Day11/Part2.cs
@@ -1,12 +1,51 @@
 ﻿namespace everybody.codes_2025.Day11;
 
-public class Part2() : BasePart(11,2,true)
+public class Part2() : BasePart(11,2)
 {
     public override string Run()
     {
-        //var input = Input();
-        //var input = InputChars();
+        var columns = Input().Select(int.Parse).ToList();
 
-        return 0.ToString();
+        //First round
+        var columnsChanged = -1;
+        var executedRounds = 0;
+
+        while (columnsChanged != 0)
+        {
+            columnsChanged = 0;
+            for (int i = 0; i < columns.Count - 1; i++)
+            {
+                if (columns[i] > columns[i + 1])
+                {
+                    columns[i]--;
+                    columns[i+1]++;
+                    columnsChanged++;
+                }
+            }
+
+            executedRounds++;
+        }
+        executedRounds--;
+
+        columnsChanged = -1;
+
+        while (columnsChanged != 0)
+        {
+            columnsChanged = 0;
+            for (int i = 0; i < columns.Count - 1; i++)
+            {
+                if (columns[i] < columns[i + 1])
+                {
+                    columns[i]++;
+                    columns[i+1]--;
+                    columnsChanged++;
+                }
+            }
+
+            executedRounds++;
+        }
+        executedRounds--;
+
+        return executedRounds.ToString();
     }
 }
--- obj/Debug/net8.0/everybody.codes-2025.AssemblyInfo.cs
+++ obj/Debug/net8.0/everybody.codes-2025.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1d49a0a4912b541109c084dbe0cea114933dfc1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bf5ebb2ea9b7e7f771b9b74f9951898580331a73")]
 [assembly: System.Reflection.AssemblyProductAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyTitleAttribute("everybody.codes-2025")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
