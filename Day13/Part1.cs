5386cd825bc800a6dde1743c3c6a04646e044367
SamBobBarnes <samuelrbarnes@outlook.com>
2025-11-19 17:55:51 -0600

    d13p1

--- Day13/Part1.cs
+++ Day13/Part1.cs
@@ -1,12 +1,34 @@
 ﻿namespace everybody.codes_2025.Day13;
 
-public class Part1() : BasePart(13,1,true)
+public class Part1() : BasePart(13,1)
 {
     public override string Run()
     {
-        //var input = Input();
-        //var input = InputChars();
+        var input = Input().Select(int.Parse).ToList();
 
-        return 0.ToString();
+        var dial = new List<int> { 1 };
+
+        var clockwise = true;
+        var indexOf1 = 0;
+        foreach (var num in input)
+        {
+            if(clockwise)
+                dial.Add(num);
+            else
+            {
+                dial.Insert(0, num);
+                indexOf1++;
+            }
+
+            clockwise = !clockwise;
+        }
+
+        var positions = dial.Count;
+        var positionsToTurn = 2025;
+
+        var finalPosition = (positionsToTurn + indexOf1) % positions;
+
+
+        return dial[finalPosition].ToString();
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿
-using everybody.codes_2025.Day12;
+using everybody.codes_2025.Day13;
 
 new Part1();
 new Part2();
